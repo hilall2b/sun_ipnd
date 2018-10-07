8548acceeb87947a1be9afcde2d167b4ff4590cb
abdulazizalawshan <abdulaziz.aloshin@gmail.com>
2018-10-07 20:12:39 +0300

    this file is filled and ready to be upload

---
+++ questions.py
@@ -0,0 +1,14 @@
+# in fibonacci numbers what is the index of 1597
+# A = an integer number
+
+
+# caesar cipher encrypt algorithm
+# input: plaintext = python, key = 5
+# output = udymts
+
+
+
+
+# caesar cipher decrypt algorithm
+# input: ciphertext = dovkdede, key = 3
+# output = alshabab
